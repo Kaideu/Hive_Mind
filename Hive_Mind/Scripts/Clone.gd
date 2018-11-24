e25b650c4f9281529570825b21bd188a859fe0ff
Kaideu <thisaccountis@yahoo.com>
2018-11-24 01:01:52 -0600

    Added and tested health bar and damage

--- Hive_Mind/Scripts/Clone.gd
+++ Hive_Mind/Scripts/Clone.gd
@@ -4,8 +4,10 @@ signal Died
 
 
 export (String) var unit_type
+var team
 var move_speed = 1
-var health = 8
+var MaxHealth = 8.0
+var health
 var attack
 var attack_speed
 var attack_range
@@ -15,41 +17,59 @@ var upgrades = 0
 var able_upgrade = true
 
 func _ready():
+	health = MaxHealth
 	match unit_type:
 		"red":
-			attack = 2
-			attack_speed = 1
+			attack = 2.0
+			attack_speed = 1.0
 			attack_range = 2
 			unit_cost = 20
 		"blue":
 			attack = "slow"
-			attack_speed = 1
+			attack_speed = 1.0
 			attack_range = 3
 			unit_cost = 30
 		"green":
 			attack = "poison"
-			attack_speed = 1
+			attack_speed = 1.0
 			attack_range = 1
 			unit_cost = 40
 		"yellow":
-			attack = -8
-			attack_speed = 1
+			attack = -8.0
+			attack_speed = 1.0
 			attack_range = 5
 			unit_cost = 100
 		"purple":
-			attack = 3 + 1
-			attack_speed = 1
+			attack = 3.0 + 1.0
+			attack_speed = 1.0
 			attack_range = 2
 			unit_cost = 60
 	
 	if unit_type != null:
 		get_node("Sprite").get_node("AnimationPlayer").play(unit_type + "_down")
 	
-	print(unit_cost)
 	
+	
+func Damaged():
+	health -= 1
+	print(health)
+	_CheckHP()
+
+func _CheckHP():
+	print("CheckingHP")
+	if health < 8:
+		$Health.show()
+	else:
+		$Health.hide()
+
+func _Died():
+	print("Drop Resource")
+	queue_free()
+
+func _process(delta):
+	if health <= 0:
+		_Died()
+	if health > 0:
+		$Health/Current.rect_scale.x = health/MaxHealth
 	
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
