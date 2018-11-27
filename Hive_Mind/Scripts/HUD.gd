de1d54c736e4a4b5e0aa065be317fd0ac42257a3
Kaideu <thisaccountis@yahoo.com>
2018-11-27 00:17:40 -0600

    Updated HUD buttons and health display to functional

--- Hive_Mind/Scripts/HUD.gd
+++ Hive_Mind/Scripts/HUD.gd
@@ -4,17 +4,30 @@ signal Damage
 signal SpawnClone(type)
 signal mouse_left
 
+signal Damage_Player
+
 var able_spawn = false
+
+var blue
+var red_
+var green
+var yellow
+var purple
+var block
+
+onready var buttons = [$Top/ButtonBlock, $Top/ButtonRed, $Top/ButtonBlue, $Top/ButtonGreen, $Top/ButtonYellow, $Top/ButtonPurple]
 
 func _ready():
 	pass
 
 
-
 func _process(delta):
 	pass
 
-
+func button_able_toggle(pressed, type):
+	for i in buttons:
+		if i != buttons[type]:
+			i.disabled = pressed
 
 func _input(event):
 	if event.is_action_pressed("Place_Piece"):
@@ -28,42 +41,77 @@ func _SpawnClone(type):
 	emit_signal("SpawnClone", type)
 
 
-func _on_ButtonBlock_toggled():
-	yield(self, "mouse_left")
-	if able_spawn:
-		_SpawnClone("block")
+func _on_ButtonBlock_toggled(pressed):
+	button_able_toggle(pressed, 0)
+	while pressed:
+		print(pressed)
+		yield(self, "mouse_left")
+		if $Top/ButtonBlock.pressed == false:
+			pressed = false
+			button_able_toggle(pressed, 0)
+			break
+		if able_spawn and pressed:
+			_SpawnClone("block")
 
 
 func _on_ButtonRed_toggled(pressed):
-	if pressed:
+	button_able_toggle(pressed, 1)
+	while pressed:
 		yield(self, "mouse_left")
-		print(pressed)
+		if $Top/ButtonRed.pressed == false:
+			pressed = false
+			button_able_toggle(pressed, 1)
+			break
 		if able_spawn and pressed:
 			_SpawnClone("red")
 
 
-func _on_ButtonBlue_toggled():
-	yield(self, "mouse_left")
-	if able_spawn:
-		_SpawnClone("blue")
+func _on_ButtonBlue_toggled(pressed):
+	button_able_toggle(pressed, 2)
+	while pressed:
+		yield(self, "mouse_left")
+		if $Top/ButtonBlue.pressed == false:
+			pressed = false
+			button_able_toggle(pressed, 2)
+			break
+		if able_spawn and pressed:
+			_SpawnClone("blue")
 
 
-func _on_ButtonGreen_toggled():
-	yield(self, "mouse_left")
-	if able_spawn:
-		_SpawnClone("green")
+func _on_ButtonGreen_toggled(pressed):
+	button_able_toggle(pressed, 3)
+	while pressed:
+		yield(self, "mouse_left")
+		if $Top/ButtonGreen.pressed == false:
+			pressed = false
+			button_able_toggle(pressed, 3)
+			break
+		if able_spawn and pressed:
+			_SpawnClone("green")
 
 
-func _on_ButtonYellow_toggled():
-	yield(self, "mouse_left")
-	if able_spawn:
-		_SpawnClone("yellow")
+func _on_ButtonYellow_toggled(pressed):
+	button_able_toggle(pressed, 4)
+	while pressed:
+		yield(self, "mouse_left")
+		if $Top/ButtonYellow.pressed == false:
+			pressed = false
+			button_able_toggle(pressed, 4)
+			break
+		if able_spawn and pressed:
+			_SpawnClone("yellow")
 
 
-func _on_ButtonPurple_toggled():
-	yield(self, "mouse_left")
-	if able_spawn:
-		_SpawnClone("purple")
+func _on_ButtonPurple_toggled(pressed):
+	button_able_toggle(pressed, 5)
+	while pressed:
+		yield(self, "mouse_left")
+		if $Top/ButtonPurple.pressed == false:
+			pressed = false
+			button_able_toggle(pressed, 5)
+			break
+		if able_spawn and pressed:
+			_SpawnClone("purple")
 
 
 func _on_HUD_mouse_entered():
@@ -74,3 +122,7 @@ func _on_HUD_mouse_entered():
 func _on_HUD_mouse_exited():
 	able_spawn = false
 	print("Left HUD")
+
+
+func _on_Button_pressed():
+	emit_signal("Damage_Player", 2)
--- Hive_Mind/Scripts/Heart.gd
+++ Hive_Mind/Scripts/Heart.gd
@@ -1,14 +1,26 @@
 extends AnimatedSprite
 
-var hits = 8
+var hits = 13
 
-export (int) var HP_range
+export (int) var HP_min
+export (int) var HP_max
+
+## needs to be a global value reference from player HP and constantly updated
+var Health = 97
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	# connect the pressed signal to the 
+	owner.connect("Damage_Player", self, "_receive_damage")
 
 func _process(delta):
 	
+	if Health < HP_min:
+		hits = 0
+	if Health >= HP_min and Health <= HP_max:
+		hits = Health - HP_min
 	animation = str(hits)
+	
+
+
+func _receive_damage(x):
+	Health -= x
